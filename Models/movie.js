8d4e19bb9b3885429ce864626fed6d6a33deda5c
Manish Kumar <manishhansalmeh@gmail.com>
2022-07-15 17:44:48 +0530

    update

--- Models/movie.js
+++ Models/movie.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const movie = new mongoose.Schema({
-  movieName: { type: String },
-  movieTitle: { type: String },
-  rating: { type: String },
-  category: { type: String },
-  price: { type: String },
+  movieName: { type: String, required: true },
+  movieTitle: { type: String, required: true },
+  rating: { type: String, required: true },
+  category: { type: String, required: true },
+  price: { type: String, required: true },
 });
 
 module.exports = mongoose.model("movie", movie);
